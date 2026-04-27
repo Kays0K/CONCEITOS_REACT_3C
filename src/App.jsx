a35f9dcc7631ed86214dceb0c6fdbf694dd14711
Kays0K <kayson.games.youtub@gmail.com>
2026-04-27 11:36:36 -0300

    React

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [carro, setCarro] = useState ('BMW')
+
+  function trocarDeCarro() {
+    if (carro === "Mustang"){
+    setCarro("BMW")
+  }
+
+    else {
+      setCarro("Mustang")
+    }
+  }
+
+  return (
+    <>
+    <h1>{carro}</h1>
+    <button onClick={trocarDeCarro}>Trocar de carro</button>
+    </>
+  )
+}
+
+export default App
